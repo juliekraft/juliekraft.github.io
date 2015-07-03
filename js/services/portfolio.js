7e2af141d58874ab3fcb5c34937b1b3597228b31
Julie Kraft <julieschmoo@gmail.com>
2015-07-03 14:58:44 -0400

    porfolio js

--- js/services/portfolio.js
+++ js/services/portfolio.js
@@ -2,7 +2,6 @@ app.factory('portfolio', ['$http', function($http) {
   return $http.get('http://juliekraft.github.io/db/data.json')
          .success(function(data) {
            return data;
-           console.log('success');
          })
          .error(function(err) {
            return err;
